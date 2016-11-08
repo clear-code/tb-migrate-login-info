71360f2108a6e798803c53e561651f4c68e46fec
YUKI Hiroshi <shimoda@clear-code.com>
2016-11-08 15:58:47 +0900

    Add sample rule

--- defaults/preferences/migrate-login-info.js
+++ defaults/preferences/migrate-login-info.js
@@ -2,4 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// pref("extensions.migrate-login-info@clear-code.com.migration.0", "pop3:mail.example.com => smtp:mail.example.com");
+
 pref("extensions.migrate-login-info@clear-code.com.debug", false);
